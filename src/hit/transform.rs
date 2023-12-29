96474070f8df168c7de71e1336d1cf1d5ad5ce19
milan <milan@milan.wtf>
2023-12-29 03:34:13 +0100

    RotationY half done

--- src/hit/transform.rs
+++ src/hit/transform.rs
@@ -26,19 +26,72 @@ impl Translation {
 
 impl Hit for Translation {
     fn hit(&self, ray: &Ray, t_interval: Interval) -> Option<HitResult> {
-        let translated_ray = Ray {
+        let ray_object_space = Ray {
             origin: ray.origin - self.offset,
             direction: ray.direction,
             time: ray.time,
-        };
-        let hit = self.object.hit(&translated_ray, t_interval);
+        }; // ray into object space
+        let hit = self.object.hit(&ray_object_space, t_interval);
 
         if let Some(mut hit) = hit {
-            hit.point += self.offset;
+            hit.point += self.offset; // point into world space
 
             Some(hit)
         } else {
             None
+        }
+    }
+
+    fn bounding_box(&self) -> &Aabb {
+        &self.bounding_box
+    }
+}
+
+pub struct RotationY {
+    sinAngle: f64,
+    cosAngle: f64,
+    object: Arc<dyn Hit>,
+    bounding_box: Aabb,
+}
+
+impl RotationY {
+    pub fn new(object: Arc<dyn Hit>, angle: f64) -> Self {
+        let angle = angle.to_radians();
+    }
+}
+
+impl Hit for RotationY {
+    fn hit(&self, ray: &Ray, t_interval: Interval) -> Option<HitResult> {
+        let ray_object_space = Ray {
+            origin: Vec3(
+                self.cosAngle * ray.origin.0 - self.sinAngle * ray.origin.2,
+                ray.origin.1,
+                self.sinAngle * ray.origin.0 + self.cosAngle * ray.origin.2,
+            ),
+            direction: Vec3(
+                self.cosAngle * ray.direction.0 - self.sinAngle * ray.direction.2,
+                ray.direction.1,
+                self.sinAngle * ray.direction.0 + self.cosAngle * ray.direction.2,
+            ),
+            time: ray.time,
+        }; // ray into object space
+        let hit = self.object.hit(&ray_object_space, t_interval);
+
+        if let Some(mut hit) = hit {
+            hit.point = Vec3(
+                self.cosAngle * hit.point.0 + self.sinAngle * hit.point.2,
+                hit.point.1,
+                -self.sinAngle * hit.point.0 + self.cosAngle * hit.point.2,
+            ); // intersection point into world space
+            hit.normal = Vec3(
+                self.cosAngle * hit.normal.0 + self.sinAngle * hit.normal.2,
+                hit.normal.1,
+                -self.sinAngle * hit.normal.0 + self.cosAngle * hit.normal.2,
+            ); // normal into world space
+
+            Some(hit)
+        } else {
+            return None;
         }
     }
 
